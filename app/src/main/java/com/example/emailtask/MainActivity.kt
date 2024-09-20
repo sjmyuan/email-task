bb63e1314eaa4fdd0f030fbdd12721869e72740b
sjmyuan <shangjiaming.yuan@gmail.com>
2024-09-20 17:32:34 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/emailtask/MainActivity.kt
+++ app/src/main/java/com/example/emailtask/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.emailtask
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.emailtask.model.App1ViewModel
@@ -17,30 +25,23 @@ import com.example.emailtask.ui.compose.theme.EmailTaskTheme
 import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val appApplication = application as AppApplication
         val app1ViewModel: App1ViewModel by viewModels {
             App1ViewModelFactory(
-                appApplication.contactRepository,
-                appApplication.scheduleRepository
+                appApplication.contactRepository, appApplication.scheduleRepository
             )
         }
 
-        val eventGeneratorRequest =
-            PeriodicWorkRequestBuilder<EventGenerator>(5, TimeUnit.MINUTES).build()
-
-        val eventSenderRequest =
-            PeriodicWorkRequestBuilder<EventSender>(1, TimeUnit.MINUTES).build()
-
-        WorkManager.getInstance(applicationContext)
-            .enqueue(listOf(eventGeneratorRequest, eventSenderRequest))
+        val intent = Intent(applicationContext, MainService::class.java)
+        startForegroundService(intent)
 
         setContent {
             EmailTaskTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     BottomNavigation(app1ViewModel)
                 }
